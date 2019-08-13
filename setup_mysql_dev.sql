a33b2fd49005df7f0048cc9ecfb804b81c8d1e99
renefibonacci660 <660@holbertonschool.com>
2019-08-13 19:48:00 +0000

    Script prepares server for project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,17 @@
+-- Prepares a MySQL server for the project by
+-- Creating new DB hbnb_dev_db and new user hbnb_dev in localhost
+-- psswd for user set to hbnb_dev_pwd
+-- grants all privileges to new user on db hbnb_dev_db
+-- grants select privileges to new user on db performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT USAGE ON *.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
