ef0d4c243303c8932f0e055ead34d5b2959f1e92
Rmolimock <495@holbertonschool.com>
2019-08-13 23:57:54 +0000

    init with kwargs if created on console with attribute key/values

--- models/base_model.py
+++ models/base_model.py
@@ -20,12 +20,16 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
-        if kwargs:
+        if len(kwargs) > 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
-                    setattr(self, key, value)
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
