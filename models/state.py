568bc0ffbf6369c4bb64366d395dd30d5ae7a4f1
renefibonacci660 <660@holbertonschool.com>
2019-08-14 23:40:24 +0000

    Added tablename and updated name attr

--- models/state.py
+++ models/state.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
