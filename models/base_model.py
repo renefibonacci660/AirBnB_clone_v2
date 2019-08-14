09c0547aa572d587349c3b80005e4e1464736fbf
Rmolimock <495@holbertonschool.com>
2019-08-14 06:08:29 +0000

    undo change to init with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -21,10 +21,6 @@ class BaseModel:
             updated_at: updated date
         """
         if len(kwargs) > 0:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
