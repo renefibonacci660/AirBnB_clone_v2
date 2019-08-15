b4abd8c2d8544de4168b68eacb959ff1b40af966
Rmolimock <495@holbertonschool.com>
2019-08-15 20:37:31 +0000

    reverted back to debug

--- models/city.py
+++ models/city.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
 """This is the city class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-import os
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    __tablename__ = "cities"
-
-    state_id = Column(String(60), ForeignKey("states.id"),
-                      nullable=False)
-
-    name = Column(String(128), nullable=False)
-
-    places = relationship("Place", backref="cities", cascade="delete")
+    state_id = ""
+    name = ""
