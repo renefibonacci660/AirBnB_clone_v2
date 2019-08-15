10f7c700b8b8cb5d4ce560de568a7f542be7f5c2
Rmolimock <495@holbertonschool.com>
2019-08-15 19:54:59 +0000

    delete methods and add declarative_base

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,16 @@
 import uuid
 import models
 from datetime import datetime
-
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
+    id = Column(String(60), nullable=False, unique=True, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -48,6 +52,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -56,7 +61,14 @@ class BaseModel:
             returns a dictionary of all the key values in __dict__
         """
         my_dict = dict(self.__dict__)
+
+        if "_sa_instance_state" in my_dict:
+            del my_dict["_sa_instance_state"]
+
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
         return my_dict
+
+    def delete(self):
+        models.storage.delete(self)
