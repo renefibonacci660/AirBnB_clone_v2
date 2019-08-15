597b0c1022921ab57701883ec6cc4940fa29bca2
Rmolimock <495@holbertonschool.com>
2019-08-15 20:40:45 +0000

    back up to #6

--- models/state.py
+++ models/state.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",
+                          backref="state",
+                          cascade="all, delete-orphan")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        @property
+        def cities():
+            return [city for city in self.cities if city.state_id == self.id]
