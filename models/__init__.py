6ea6898ab781b6d04438c435cd8f1aca7d6fdd7d
Rmolimock <495@holbertonschool.com>
2019-08-15 19:44:13 +0000

    create objects with attributes

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,8 +11,5 @@ from models.review import Review
 import os
 
 
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    storage = DBStorage()
-else:
-    storage = FileStorage()
+storage = FileStorage()
 storage.reload()
