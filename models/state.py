11740f30f34af87bbe15e39317eac7fdec232d9c
Rmolimock <495@holbertonschool.com>
2019-08-15 20:00:00 +0000

    changed attributes into table columns

--- models/state.py
+++ models/state.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import json
+from os import getenv
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",
+                          backref=backref("State", cascade="delete"))
+
+    if (getenv('HBNB_TYPE_STORAGE') != "db"):
+        @property
+        def cities(self):
+            """returns list of city
+            instances with
+            matching state_id
+            """
+            cityObjs = models.storage.all('City').values()
+            return [c for c in cityObjs if c.state_id == self.id]
