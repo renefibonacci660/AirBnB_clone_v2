724b66f58cbc5eccc0d64a47041316349a2c4032
Rmolimock <495@holbertonschool.com>
2019-08-15 20:05:01 +0000

    rene fixed the localhost access bug

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -11,5 +12,8 @@ from models.review import Review
 import os
 
 
-storage = FileStorage()
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
