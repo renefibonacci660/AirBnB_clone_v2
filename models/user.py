41107dbbf4729ef47a7680b6a69060bb0fd8122d
renefibonacci660 <660@holbertonschool.com>
2019-08-15 18:07:37 +0000

    Added relationship for reviews and user

--- models/user.py
+++ models/user.py
@@ -21,6 +21,7 @@ class User(BaseModel, Base):
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
         places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", casacade="delete")
     else:
         email = ""
         password = ""
