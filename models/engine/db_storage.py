e4a696142d50f27efcaf921d08de0be5080bcd93
Rmolimock <495@holbertonschool.com>
2019-08-15 00:12:44 +0000

    pep8 compliant style

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -41,18 +41,24 @@ class DBStorage:
         if os.getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
-        def all(self, cls=None):
-            '''
-            '''
-            all_classes = ['User', 'State', 'City', 'Amenity', 'Place', 'Review']
-            obj_dict, obj_list = {}, []
-            if cls is None:
-                for each_class in all_classes:
-                    for obj in self.__session.query(eval(each_class)):
-                        obj_list.append(obj)
-            else:
-                for obj in self.__session.query(eval(cls)):
+    def all(self, cls=None):
+        '''
+        '''
+        all_classes = ['User', 'State', 'City', 'Amenity', 'Place', 'Review']
+        obj_dict, obj_list = {}, []
+        if cls is None:
+            for each_class in all_classes:
+                for obj in self.__session.query(eval(each_class)):
                     obj_list.append(obj)
-            for obj in obj_list:
-                obj_dict[type(obj).__name__ + "." + str(obj.id)] = obj
-            return obj_dict
+        else:
+            for obj in self.__session.query(eval(cls)):
+                obj_list.append(obj)
+        for obj in obj_list:
+            obj_dict[type(obj).__name__ + "." + str(obj.id)] = obj
+        return obj_dict
+
+    def new(self, obj):
+        """Add an object to the database session"""
+        if obj:
+            self.__session.add(obj)
+  
