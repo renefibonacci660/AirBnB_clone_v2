22278c2364b447c7a7a1643e8b5e175bdf30b786
renefibonacci660 <660@holbertonschool.com>
2019-08-15 01:41:36 +0000

    Added relationship to Places in City class

--- models/city.py
+++ models/city.py
@@ -2,6 +2,8 @@
 """This is the city class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import relationship
+import os
 
 
 class City(BaseModel, Base):
@@ -11,7 +13,11 @@ class City(BaseModel, Base):
         name: input name
     """
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey('states.id'),
-                      nullable=False)
-    name = Column(String(128), nullable=False)
-
+    if os.getenv("HBNB_FILE_STORAGE") == "db":
+        state_id = Column(String(60), ForeignKey("states.id"),
+                          nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        state_id = ""
+        name = ""
