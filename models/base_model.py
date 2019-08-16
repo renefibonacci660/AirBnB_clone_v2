794e792a504c24cc7c0d7bdd6ba5fe5154c12689
Rmolimock <495@holbertonschool.com>
2019-08-16 02:02:29 +0000

    removed old copy of models.save() from init function

--- models/base_model.py
+++ models/base_model.py
@@ -36,7 +36,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
