3001f4da78804a05c4982cafe4ac788b70e4b3ee
Rmolimock <495@holbertonschool.com>
2019-08-16 03:23:23 +0000

    made executable

--- console.py
+++ console.py
@@ -14,7 +14,7 @@ from shlex import split
 
 
 class HBNBCommand(cmd.Cmd):
-    """this class is entry point of the command interpreter
+    """this class is the entry point of the command interpreter
     """
     prompt = "(hbnb) "
     all_classes = {"BaseModel", "User", "State", "City",
